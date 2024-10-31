2704fb58b3fe44e0de70089edf4715544c3e6c8b
BellamyDev <johnbellamydev@gmail.com>
2024-10-31 03:14:18 -0500

    Added General Structure

--- accountMaker.java
+++
@@ -1,9 +0,0 @@
-package Chat;
-
-//import java.io.*;
-import java.util.Scanner;
-
-public class accountMaker {
-    Scanner scan = new Scanner(System.in);
-    
-}
--- userAuth.java
+++
@@ -1,8 +0,0 @@
-package Chat;
-
-//import java.io.*;
-import java.util.Scanner;
-
-public class userAuth {
-    Scanner scan = new Scanner(System.in);
-}
