0e3a6da28caa6ac2494567243ba93850516d67e7
joesevv <joseph.severino1@gmail.com>
2024-11-02 16:31:55 -0500

    Create Profile.js

---
+++ client/src/Profile.js
@@ -0,0 +1,12 @@
+function Profile() {
+    return (
+        <div>
+            <h2>Your Profile</h2>
+            <p>Name: [User Name]</p>
+            <p>Email: [User Email]</p>
+            {/* Can add more stuff */}
+        </div>
+    );
+}
+
+export default Profile;
